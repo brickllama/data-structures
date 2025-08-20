4d701e35488ee1ae904bdf95e3c6c852a7a26a4d
Nathaniel Nicholas <nn222qn@student.lnu.se>
2025-08-20 15:04:34 +0200

    Renamed to match library name

--- data_structures.c
+++ data_structures.c
@@ -1,4 +1,4 @@
-#include "library.h"
+#include "data_structures.h"
 
 #include <stdio.h>
 
---
+++ data_structures.h
@@ -0,0 +1,6 @@
+#ifndef DATA_STRUCTURES_H
+#define DATA_STRUCTURES_H
+
+void hello(void);
+
+#endif // DATA_STRUCTURES_H
